e0a3316ebb8e47b01413ec453111898c677cefb5
Riku <134082872+rigozu9@users.noreply.github.com>
2024-05-14 10:58:54 +0300

    9.10 done

--- patientor_backend/src/index.ts
+++ patientor_backend/src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosisRouter from './routes/diagnosis';
 
 const app = express();
-app.use(cors());  // This will allow all domains to access your backend
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3000;
@@ -11,6 +12,8 @@ app.get('/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosisRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
